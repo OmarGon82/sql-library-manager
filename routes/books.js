7a786a9b0e7336ee6ae1f2541aedb9b9bd4e62fc
Omar Gonzalez <gonzalezoe82@gmail.com>
2020-02-09 10:23:43 +0900

    reworked delete to match project mock-up

--- routes/books.js
+++ routes/books.js
@@ -29,7 +29,7 @@ router.post('/', handleAsync(async (req, res) => {
     let book;
     try {
         book = await Book.create(req.body);
-        res.redirect("/books/" + book.id);
+        res.redirect("/books");
     } catch (error) {
         if( error.name === "SequelizeValidationError") {
             book = await Book.build(req.body);
